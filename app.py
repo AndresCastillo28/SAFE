687ee4630a716171af093924b14310a85c645488
Andres Castillo <ansa2000@hotmail.es>
2022-09-10 18:05:28 -0500

    Connection Database

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from routes.users import users
+from config.db import DATABASE_CONNECTION_URI
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/')
 def index():
     return {'status': 'API running'}
 
+SQLAlchemy(app)
+
+app.register_blueprint(users)
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True)   
---
+++ config/db.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.environ['MYSQL_USER']
+host = os.environ['MYSQL_HOST']
+database = os.environ['MYSQL_DATABASE']
+
+
+DATABASE_CONNECTION_URI = f'mysql://{user}@{host}/{database}'
+
+print('Database is connected!!!')
